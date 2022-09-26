989f096b7ca99b0d12ab7cf3e5c5104b73369b27
Fahimlica <fahim.faizul2002@gmail.com>
2022-09-26 13:20:37 +0600

    somechange

--- src/Componants/Cart/Cart.js
+++ src/Componants/Cart/Cart.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Cart = ({ cart }) => {
     return (
         <div>
-            <h2>Order Summary</h2>
+            <h2>Order Summary33</h2>
             <h3>Selected Items: {cart.length} </h3>
         </div>
     );
